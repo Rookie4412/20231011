fe68149c87f5f3e663174cae29723a6a7efe853c
Rookie4412 <4412hank@gmail.com>
2023-10-11 12:22:45 +0800

    ttt

---
+++ main.js
@@ -0,0 +1,16 @@
+//export 具名匯出 
+export const a = 2;
+
+export const text = "bey";
+
+const obj ={
+    arr : ["pop",18,]
+}
+
+function fu(){
+    console.log("xxx")
+}
+//預設匯出 只能一個
+export default fu;
+
+
---
+++ main2.js
@@ -0,0 +1,11 @@
+// import { a } from "./main.js";
+// import { text } from "./main.js";
+
+import { a,text, } from "./main.js";
+
+console.log(a)
+console.log(text)
+
+import asdfqer from "./main.js";
+// console.log(asdfqer)
+asdfqer()
